17215d1381684df71d20a31742a7030e9078eecb
Bendi11 <bkliebmann@gmail.com>
2023-07-24 19:38:24 -0700

    Added Payload impl for Vec<u8>

--- sendy-framework/src/net/packet.rs
+++ sendy-framework/src/net/packet.rs
@@ -1,16 +1,24 @@
+use std::io::{Cursor, Read, Write};
+
+use byteorder::{LittleEndian, ReadBytesExt, LE, WriteBytesExt};
+
 
 #[repr(u8)]
 #[derive(Clone, Copy, Debug, PartialEq, Eq)]
 pub enum PacketKind {
     Conn = 0,
     Ack = 1,
+
+    Test = 2,
 }
 
 #[derive(Clone, Copy, Debug)]
 pub struct PacketHeader {
     pub kind: PacketKind,
-    pub msgid: u16,
-    pub seqno: u32,
+    /// ID of the message, rolls over to 1 after passing 255
+    pub msgid: u8,
+    /// Offset into message buffer (in blocks) to place the payload bytes
+    pub msgoff: u32,
 }
 
 #[derive(Clone, Copy, Debug)]
@@ -26,23 +34,67 @@ pub trait PacketPayload: Sized {
     
     /// Consume bytes from the given buffer to parse an instance of `Self`, returning the remaining
     /// portion of the buffer and the instance or an error
-    fn parse(buf: &[u8]) -> Result<(&[u8], Self), Self::Error> where
-        PacketParseError: From<Self::Error>;
+    fn parse<R: Read>(buf: R) -> Result<Self, Self::Error>;
     
     /// Write the representation of this payload to a buffer of bytes
-    fn write(&self, buf: &mut Vec<u8>);
+    fn write<W: Write>(&self, buf: W) -> Result<(), Self::Error>;
 }
 
 
 impl PacketPayload for () {
     type Error = std::convert::Infallible;
 
-    fn parse(buf: &[u8]) -> Result<(&[u8], Self), Self::Error> {
-        Ok((buf, ()))
+    fn parse<R: Read>(_: R) -> Result<Self, Self::Error> { Ok(()) }
+    fn write<W: Write>(&self, _: W) -> Result<(), Self::Error> { Ok(()) }
+}
+
+impl PacketPayload for Vec<u8> {
+    type Error = std::io::Error;
+
+    fn parse<R: Read>(mut read: R) -> Result<Self, Self::Error> {
+        let mut this = Vec::new();
+        read.read_to_end(&mut this)?;
+        Ok(this)
+    }
+    
+    fn write<W: Write>(&self, mut buf: W) -> Result<(), Self::Error> {
+        buf.write_all(&self)
+    }
+}
+
+impl PacketPayload for PacketHeader {
+    type Error = PacketHeaderParseError;
+
+    fn parse<R: Read>(mut buf: R) -> Result<Self, Self::Error> {
+        let kind = buf.read_u8()?;
+        let kind = PacketKind::try_from(kind)?;
+        let msgid = buf.read_u8()?;
+        let msgoff = buf.read_u32::<LE>()?;
+
+        Ok(Self {
+            kind,
+            msgid,
+            msgoff,
+        })
     }
 
-    fn write(&self, buf: &mut Vec<u8>) {
+    fn write<W: Write>(&self, mut buf: W) -> Result<(), Self::Error> {
+        buf.write_u8(self.kind as u8)?;
+        buf.write_u8(self.msgid)?;
+        buf.write_u32::<LE>(self.msgoff)?;
 
+        Ok(())
+    }
+}
+
+impl PacketKind {
+    /// If the packet is a control packet that will not send more bytes
+    pub const fn is_control(&self) -> bool {
+        match self {
+            Self::Conn 
+            | Self::Ack => true,
+            _ => false,
+        }
     }
 }
 
@@ -64,6 +116,15 @@ pub struct PacketKindParseErr(pub u8);
 
 #[derive(Debug, thiserror::Error)]
 pub enum PacketParseError {
-    #[error("Failed to parse kind: {0}")]
+    #[error("Failed to parse packet header: {0}")]
+    Header(#[from] PacketHeaderParseError),
+}
+
+#[derive(Debug, thiserror::Error)]
+pub enum PacketHeaderParseError {
+    #[error("Failed to parse packet kind: {0}")]
     Kind(#[from] PacketKindParseErr),
+
+    #[error("I/O Error: {0}")]
+    IO(#[from] std::io::Error),
 }
--- sendy-framework/src/net/sock.rs
+++ sendy-framework/src/net/sock.rs
@@ -1,7 +1,38 @@
+use std::io::Cursor;
 
+use tokio::net::UdpSocket;
+
+use super::packet::{PacketHeader, PacketPayload};
+
+
+const MAX_IN_TRANSIT_MSG: usize = 5;
+const MAX_PACKET_SZ: usize = 500;
 
 /// Minimal reliability layer over a UDP connection
+#[derive(Debug)]
 struct ReliableSocket {
-    
+    sock: UdpSocket,
+    recv: WaitingPackets,
+    tx: WaitingPackets,
 }
 
+#[derive(Debug)]
+struct WaitingPackets {
+    pub waiting_pkts: [Vec<u8> ; MAX_IN_TRANSIT_MSG],
+    pub msg_block_counts: [u32 ; MAX_IN_TRANSIT_MSG],
+    pub waiting_pkt_header: [PacketHeader ; MAX_IN_TRANSIT_MSG],
+    pub waiting_pkt_ids: [u8 ; MAX_IN_TRANSIT_MSG],
+}
+
+impl ReliableSocket {
+    pub async fn recv(&self) -> Vec<u8> {
+        let mut buf = [0u8 ; MAX_PACKET_SZ];
+        loop {
+            let pkt = self.sock.recv(&mut buf).await.unwrap();
+            let mut buf = Cursor::new(&buf);
+            let header = PacketHeader::parse(&mut buf).unwrap();
+            
+
+        }
+    }
+}
