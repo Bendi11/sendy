5578167a762bad91bc4d55b66e9f988b32a53a9e
Bendi11 <bkliebmann@gmail.com>
2023-07-30 20:08:14 +0000

    Added generic implementation of To and From Bytes for Vec

--- sendy-framework/src/ser.rs
+++ sendy-framework/src/ser.rs
@@ -19,3 +19,36 @@ pub trait FromBytes: Sized {
     /// instance of `Self`
     fn parse<R: Buf>(buf: R) -> Result<Self, std::io::Error>;
 }
+
+type VecToBytesLenType = u32;
+
+impl<T: ToBytes> ToBytes for Vec<T> {
+    fn write<B: BufMut>(&self, mut buf: B) {
+        buf.put_u32_le(self.len() as VecToBytesLenType);
+        for elem in self.iter() {
+            elem.write(&mut buf);
+        }
+    }
+
+    fn size_hint(&self) -> Option<usize> {
+        let elements = self
+            .iter()
+            .map(|elem| elem.size_hint())
+            .fold(Some(0usize), |acc, elem| if let (Some(acc), Some(elem)) = (acc, elem) {
+                Some(acc + elem)
+            } else {
+                None
+            });
+
+        elements.map(|sz| sz + std::mem::size_of::<VecToBytesLenType>())
+    }
+}
+
+impl<T: FromBytes> FromBytes for Vec<T> {
+    fn parse<R: Buf>(mut buf: R) -> Result<Self, std::io::Error> {
+        let len: VecToBytesLenType = buf.get_u32_le();
+        (0..len)
+            .map(|_| T::parse(&mut buf))
+            .collect::<Result<Self, _>>()
+    }
+}
